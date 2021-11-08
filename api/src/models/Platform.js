4bc44002c96e5f3ac4f0b655aae28d70f7a0ddea
eliasdbr <eliasdbr@outlook.com>
2021-11-08 19:21:00 -0300

    DB Models Completed

---
+++ api/src/models/Platform.js
@@ -0,0 +1,19 @@
+// -- Platform model --
+
+const { DataTypes } = require('sequelize');
+// We export a function that defines the model.
+// We inject it's conection to sequelize later.
+module.exports = (sequelize) => {
+	sequelize.define('Platform', {
+		id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+			primaryKey: true,
+		},
+		name: {
+			type: DataTypes.STRING,
+		},
+	},{
+		timestamps: false
+	});
+};
--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -1,12 +1,40 @@
+// -- Videogame model --
+
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+// We export a function that defines the model.
+// We inject it's conection to sequelize later.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+	sequelize.define('Videogame', {
+		id: {
+			type: DataTypes.INTEGER,
+			autoIncrement: true,
+			primaryKey: true,
+			// Getter. Inserts the character prefix before returning the ID
+			get() {
+				return `L${this.getDataValue('id')}`;
+			},
+			// Setter. Extracts the character prefix before storing the ID
+			set(value) {
+				this.setDataValue('id', value.slice(1) * 1);
+			}
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		release_date: {
+			type: DataTypes.STRING,
+		},
+		rating: {
+			type: DataTypes.STRING,
+		},
+		// Instead of using a 'Platforms' attribute, I'll create dedicated
+		// model for it.
+	},{
+		timestamps: false
+	});
 };
