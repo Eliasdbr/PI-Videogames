3670fc3a7f37dd3e1601ae30813af13370c23ca2
eliasdbr <eliasdbr@outlook.com>
2021-11-08 19:25:01 -0300

    Routing files created

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,5 +8,50 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
+// - - GET /videogames
+// -Front requests a list of 100 games, 
+//  each containing: {id, name, image_url, genres[]}
+// - - GET /videogames?search={name}
+// -Front request a list of 15 games whose names match the string
+//  sent via the query parameter 'search'
+router.get(
+	'/videogames',
+	(req,res) => {
+		if (req.query.search) res.status(200).json({msg: req.query.search});
+		else res.status(200).json({msg: 'Hola mundo!'});
+	}
+);
+
+// - - GET /videogame/{id}
+// - Front requests all details from the game specified by the
+//   route param 'id'
+router.get(
+	'/videogame/:id',
+	(req,res) => {
+		res.status(200).json({msg: req.params.id});
+	}
+);
+
+
+// - - GET /genres
+// - Front requests a list of all genres stored in the DB
+router.get(
+	'/genres',
+	(req,res) => {
+		res.status(200).json({msg: 'Hola generos!'});
+	}
+);
+
+
+// - - POST /videogame
+// - Front requests writing data in our DB, with the data sent
+//   via the body.
+router.post(
+	'/videogame',
+	(req,res) => {
+		res.status(200).json(req.body);
+	}
+);
+
 
 module.exports = router;
---
+++ api/src/utils/utils.js
@@ -0,0 +1,30 @@
+// All the logic for handling information goes here
+//
+
+/*
+
+*/
+
+module.exports = {
+	getGamesDB: function() {
+	},
+	
+	getGamesAPI: function() {
+	},
+	
+	postGame: function() {
+	},
+	
+	getGenresDB: function() {
+	},
+	
+	getGenresAPI: function() {
+	},
+	
+	getPlatformsDB: function() {
+	},
+	
+	getPlatformsAPI: function() {
+	},
+
+}
