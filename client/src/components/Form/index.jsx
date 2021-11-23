b745d7a31182bce9453683b6e9b65c47c59b4d90
eliasdbr <eliasdbr@outlook.com>
2021-11-23 04:52:02 -0300

    Front: Submit Design Complete.

--- client/src/components/Form/index.jsx
+++ client/src/components/Form/index.jsx
@@ -50,6 +50,7 @@ export default function Form( /* { prop1, prop2, prop3... } */ ){
 			// Submit action
 			dispatch(postGame(input));
 		}
+		else alert('You have required fields to complete!');
 	}
 	// Update error message
 	function updateErrorMsg() {
@@ -115,96 +116,112 @@ export default function Form( /* { prop1, prop2, prop3... } */ ){
 	else return (
 		<form onSubmit={submitHandle} className={style.component}>
 			<h2>Submit your Game to our Database.</h2>
+			<div className={style.twoHalves}>
+			<div className={style.half1}>
 {/*** Name Field ***/}
-			<label className={
-				!regexNotEmpty.test(input.name) 
-				? style.error : ''
-			}>Name*:</label>
-			<input className={
-					!regexNotEmpty.test(input.name)
-					? style.inputError : style.inputNorm
-				}
-				type='text' name='name' placeholder='Type here the Name of your Game.'
-				required onChange={changeHandle} maxLength='256'
-			></input>
+				<label className={
+					!regexNotEmpty.test(input.name) 
+					? style.error : ''
+				}>Name*:</label>
+				<input className={
+						!regexNotEmpty.test(input.name)
+						? style.inputError : style.inputNorm
+					}
+					type='text' name='name' placeholder='Type here the Name of your Game.'
+					required onChange={changeHandle} maxLength='256'
+				></input>
 {/*** Description Field ***/}
-			<label className={
-				!regexNotEmpty.test(input.desc) 
-				? style.error : ''
-			}>Description*:</label>
-			<textarea className={
+				<label className={
 					!regexNotEmpty.test(input.desc) 
-					? style.inputError : style.inputNorm
-				}
-				name='desc' placeholder="Type here your Game's description."
-				required onChange={changeHandle} rows='6'
-				maxLength='1024'
-			></textarea>
+					? style.error : ''
+				}>Description*:</label>
+				<textarea className={
+						!regexNotEmpty.test(input.desc) 
+						? style.inputError : style.inputNorm
+					}
+					name='desc' placeholder="Type here your Game's description."
+					required onChange={changeHandle} rows='6'
+					maxLength='1024'
+				></textarea>
 {/*** Release Date Field***/}
-			<label>Release Date:</label>
-			<input className={style.inputNorm} type='date' name='date' 
-				onChange={changeHandle} 
-			></input>
+				<div className={style.rateDate}>
+					<div className={style.inputCont}>
+						<label>Release Date:</label>
+						<input className={style.inputNorm+' '+style.date} 
+							type='date' name='date' onChange={changeHandle} 
+						></input>
+					</div>
 {/*** Rating Field ***/}
-			<label>Rating: {input.rate ? input.rate+'/5' : 'N/A'}</label>
-			<input className={style.inputNorm} type='range' name='rate'
-				onChange={changeHandle} min='0' max='5'
-			></input>
+					<div className={style.inputCont}>
+						<label>Rating: {input.rate ? input.rate+'/5' : 'N/A'}</label>
+						<input className={style.inputNorm} type='range' name='rate'
+							onChange={changeHandle} min='0' max='5'
+						></input>
+					</div>
+				</div>
 {/*** Image Field ***/}
-			<label className={
-				!regexUrl.test(input.bg_url) 
-				? style.error : ''
-			}>Image URL:</label>
-			<input className={
-					!regexUrl.test(input.bg_url)
-					? style.inputError : style.inputNorm
-				}
-				type='text' name='bg_url' 
-				placeholder='You can choose an URL for the Image of yout Game.'
-				onChange={changeHandle} maxLength='256'
-			></input>
-			<label>Image Preview:</label>
-			<img className={style.imgPreview} src={input.bg_url || defImg}
-				width='300' height='180'/>
-{/*** Genres Selection ***/}
-			<label>Genres:</label>
-			<div className={style.tagsField}>
-				{input.genres?.map(
-					genre => (
-						<button key={genre} onClick={() => deleteFromList('genres',genre)}
-						>{genres?.find(g => g.id === genre).name} [x]</button>
-					)
-				)}
-				<select name='genres' onChange={changeHandle} value='0'>
-					<option value='0'>Add a Genre</option>
-					{genres?.map(genre => (
-						<option key={genre.id} value={genre.id} >{genre.name}</option>
-					))}
-				</select>
-			</div>
-{/*** Platforms Selection ***/}
-			<label className={ !input.platforms.length ? style.error : '' }
-			>Platforms*:</label>
-			<div className={style.tagsField}>
-				{input.platforms?.map(
-					platform => (
-						<button key={platform} onClick={
-							() => deleteFromList('platforms',platform)
+				<div className={style.inputCont}>
+					<label className={
+						!regexUrl.test(input.bg_url) 
+						? style.error : ''
+					}>Image URL:</label>
+					<input className={
+							!regexUrl.test(input.bg_url)
+							? style.inputError : style.inputNorm
 						}
-						>{platforms?.find(p => p.id === platform).name} [x]</button>
-					)
-				)}
-				<select name='platforms' onChange={changeHandle} value='0'>
-					<option value='0'>Add a Platform</option>
-					{platforms?.map(platform => (
-						<option key={platform.id} value={platform.id}
-						>{platform.name}</option>
-					))}
-				</select>
+						type='text' name='bg_url' 
+						placeholder='You can choose an URL for the Image for your Game.'
+						onChange={changeHandle} maxLength='256'
+					></input>
+					<label>Image Preview:</label>
+					<img className={style.imgPreview} src={input.bg_url || defImg}
+						width='300' height='180'/>
+				</div>
+			</div>
+{/*** Genres Selection ***/}
+			<div className={style.half2}>
+				<label>Genres:</label>
+				<div className={style.tagsField}>
+					{input.genres?.map(
+						genre => (
+							<button key={genre} onClick={() => deleteFromList('genres',genre)}
+							>{genres?.find(g => g.id === genre).name} [x]</button>
+						)
+					)}
+					<select name='genres' onChange={changeHandle} value='0'>
+						<option value='0'>Add a Genre</option>
+						{genres?.map(genre => (
+							<option key={genre.id} value={genre.id} >{genre.name}</option>
+						))}
+					</select>
+				</div>
+{/*** Platforms Selection ***/}
+				<label className={ !input.platforms.length ? style.error : '' }
+				>Platforms*:</label>
+				<div className={style.tagsField}>
+					{input.platforms?.map(
+						platform => (
+							<button key={platform} onClick={
+								() => deleteFromList('platforms',platform)
+							}
+							>{platforms?.find(p => p.id === platform).name} [x]</button>
+						)
+					)}
+					<select name='platforms' onChange={changeHandle} value='0'>
+						<option value='0'>Add a Platform</option>
+						{platforms?.map(platform => (
+							<option key={platform.id} value={platform.id}
+							>{platform.name}</option>
+						))}
+					</select>
+				</div>
+			</div>
 			</div>
 {/*** Submit Button ***/}
-			<button onClick={submitHandle} disabled={!!errorMsg}>
-				<h3>Submit Game</h3>
+			<hr/>
+			<button onClick={submitHandle} disabled={!!errorMsg}
+				className={style.submit}>
+				<h2>Submit Game</h2>
 			</button>
 {/*** Error Message ***/}
 			<p className={style.error} key='errorMsg' >{errorMsg}</p>
